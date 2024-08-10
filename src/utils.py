e4e30ca9bfffafa1cac2af0d8d7fe55335582ac1
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-10 11:34:53 -0700

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,8 @@
 # Miscellaneous utility functions.
+import os
+
+def setup_environment():
+    os.environ["OPENAI_API_KEY"] = Config.OPENAI_API_KEY
+
+def log_to_console(message):
+    print(message)
